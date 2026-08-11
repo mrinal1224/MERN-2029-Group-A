890db3684807419b44c610ef04ee15742c6088d6
mrinal1224 <iammrinalkc@gmail.com>
2026-08-11 11:03:14 +0530

    route paramters

--- class-2(Introduction to express)/server/index.js
+++ class-2(Introduction to express)/server/index.js
@@ -7,10 +7,23 @@ const app = express();
 // update a resource - http method - put , patch
 // delete a resource - http method - delete
 
-app.get("/", (req, res) => {
-  res.send("This is an Express Server");
+const courses = [
+  { id: 1, courseName: "Java", instructor: "Priyansh" },
+  { id: 2, courseName: "Python", instructor: "Aadrita" },
+  { id: 3, courseName: "DBMS", instructor: "Vinayak" },
+];
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
-app.listen(8002, () => {
-  console.log("Server Started at port 8002");
+app.get("/courses/:id", (req, res) => {
+  let course = courses.find((course) => course.id === parseInt(req.params.id));
+  res.send(course);
+});
+
+// get a unique course
+
+app.listen(8004, () => {
+  console.log("Server Started at port 8004");
 });
